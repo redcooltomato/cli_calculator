2b725a4493eb83edd9678f4e7ed7455d6e729e3d
redcooltomato <redcooltomato@gmail.com>
2026-03-09 12:29:54 +0300

    tokenizing outsorced to function for the sake of readability

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::{
 enum Token {
     Number(i64),
     Operator(String),
-    Undefined,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -17,37 +16,27 @@ enum BufferMode {
     Empty,
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-
-    if (args.len() <= 1) {
-        return;
-        // todo: do --help stuff, prolly run this when expression is invalid
-    }
-
-
-    let expr: String = args[1..].join("");
-    let mut to_parse: Vec<Token> = Vec::new();
+fn tokenize(expr: &str) -> VecDeque<Token> { // todo add actual tokens (math functions matcher or smth)
+    let mut to_parse: VecDeque<Token> = VecDeque::new();
     let mut buffer: String = String::new();
     let mut buffer_mode: BufferMode = BufferMode::Empty;
 
     for char in expr.chars() {
         match char {
             '(' | ')' => {
-                to_parse.push(Token::Operator(char.to_string()));
+                to_parse.push_back(Token::Operator(char.to_string()));
             },
             '0'..='9' => {
-                if buffer_mode != BufferMode::Number && buffer_mode != BufferMode::Empty {
-                    to_parse.push(Token::Operator(buffer));
+                if buffer_mode == BufferMode::Operator {
+                    to_parse.push_back(Token::Operator(buffer));
                     buffer = String::new();
                 }
                 buffer.push(char);
                 buffer_mode = BufferMode::Number;
             },
             _ => {
-                if buffer_mode != BufferMode::Operator && buffer_mode != BufferMode::Empty {
-                    to_parse.push(Token::Number(buffer.parse().unwrap())); // unsafe as hell
+                if buffer_mode == BufferMode::Number {
+                    to_parse.push_back(Token::Number(buffer.parse().unwrap())); // unsafe as hell
                     buffer.clear();
                 }
                 buffer.push(char);
@@ -56,8 +45,36 @@ fn main() {
         }
     }
 
+    if !buffer.is_empty() {
+        match buffer_mode {
+            BufferMode::Number => {
+                to_parse.push_back(Token::Number(buffer.parse().unwrap()));
+            },
+            BufferMode::Operator => {
+                to_parse.push_back(Token::Operator(buffer));
+            }
+            _ => (),
+        }
+    }
+
+    to_parse
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+
+    if args.len() <= 1 {
+        return;
+        // todo: do --help stuff, prolly run this when expression is invalid
+    }
+
+
+    let expr: String = args[1..].join("");
+    let mut to_parse: VecDeque<Token> = tokenize(&expr);
+
     let mut calculations_queue: VecDeque<i64> = VecDeque::new();
     for token in to_parse {
-        // wait of the world
+        println!("{:?}", token);
     }
 }
