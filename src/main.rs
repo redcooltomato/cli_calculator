c4f785cefb1f5a8286935a58f39057c2b2c90927
redcooltomato <redcooltomato@gmail.com>
2026-06-02 23:05:46 +0300

    working token system with brackets

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use std::{env, collections::HashSet};
 
 
-fn split(expr: String) -> Vec<String> {
-    let blocks = ["+", "-", "*", "/", "(", ")"];
-    let hs = HashSet::from(blocks);
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+enum TokenSpec {
+    Number,
+    Operator,
+    Bracket,
+    None,
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct Token {
+    spec: TokenSpec,
+    cont: String,
+}
+
+fn split(expr: String) -> Vec<Token> {
+    println!("{:?}", expr);
+    let bracks = ['(', ')'];
 
     #[derive(PartialEq)]
     enum BuffMode {
@@ -11,27 +26,63 @@ fn split(expr: String) -> Vec<String> {
         Ops,
         None,
     }
+
+    fn buffmodeToSpec(buffmode: BuffMode) -> TokenSpec {
+        if buffmode == BuffMode::Digs { TokenSpec::Number } 
+        else if buffmode == BuffMode::Ops { TokenSpec::Operator }
+        else { TokenSpec::None }
+    }
+
     let mut buf = String::new();
     let mut buffmode: BuffMode = BuffMode::None;
 
-    let mut result: Vec<String> = Vec::new();
+    let mut result: Vec<Token> = Vec::new();
     for c in expr.chars().chain(" ".chars()) {
         if c == ' ' {
-            result.push(buf.clone());
+            if !buf.is_empty() && buffmode != BuffMode::None {
+                result.push(Token {
+                     spec: (buffmodeToSpec(buffmode)), cont: buf
+                    });
+            }
             break;
         }
+
+        if bracks.contains(&c) {
+            if !buf.is_empty() {
+                result.push(Token {
+                     spec: buffmodeToSpec(buffmode), cont: buf
+                    });
+                buf = String::new();
+            }
+
+            result.push(Token {
+                 spec: TokenSpec::Bracket, cont: c.to_string()
+                });
+                buffmode = BuffMode::None;
+            buf = String::new();
+            continue;
+        }
+
         match buffmode {
             BuffMode::Digs => {
                 if !c.is_ascii_digit() {
-                    result.push(buf.clone());
-                    buf.clear();
+                    if !buf.is_empty() {
+                        result.push(Token {
+                     spec: buffmodeToSpec(buffmode), cont: buf
+                    });
+                    }
+                    buf = String::new();
                     buffmode = BuffMode::Ops;
                 }
             },
             BuffMode::Ops => {
                 if c.is_ascii_digit() {
-                    result.push(buf.clone());
-                    buf.clear();
+                    if !buf.is_empty() {
+                        result.push(Token {
+                     spec: buffmodeToSpec(buffmode), cont: buf
+                    });
+                    }
+                    buf = String::new();
                     buffmode = BuffMode::Digs;
                 }
             },
