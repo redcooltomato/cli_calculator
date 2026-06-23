67c9b061e5f24baf61fc03e6099d4281b31eeb77
redcooltomato <redcooltomato@gmail.com>
2026-06-23 12:54:16 +0300

    added help

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,16 @@ mod tests;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        println!("no args!");
+        println!("No expression to calculate was provided.\nRun '{} help' to see available operators.", 
+        args[0].split("\\").map(|sl| sl.split("/")).last().unwrap().last().unwrap());
         return; 
     }
     let expr = args[1..].join("");
+
+    if expr.to_lowercase() == "help".to_string() {
+        println!("Calculates an expression provided.\nAvailable operators:\nx + y, x - y, x * y, x / y,\nx ^ y, sqrt x, y rt x,\nsin x, cos x, tan x, asin x, acos x, atan x.");
+        return;
+    }
 
     let answer =  match calculate_expression(&expr) {
         Ok(ans) => ans,
